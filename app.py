1cf528613ff2c92ea8f2ad287a18082ae285f2a6
epachecoosnaya <epachecoosnaya@gmail.com>
2026-01-03 19:03:00 -0600

    Login demo funcional

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return render_template("index.html")
+    return redirect(url_for("login"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        usuario = request.form["usuario"]
+        password = request.form["password"]
+
+        # Login DEMO (luego DB real)
+        if usuario == "demo" and password == "1234":
+            return redirect(url_for("dashboard"))
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run()
