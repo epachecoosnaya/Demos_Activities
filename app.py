56382c7849f5c5f2e08776be8b426c58d80c02fd
epachecoosnaya <epachecoosnaya@gmail.com>
2026-01-03 18:53:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return "<h1>Portal de Actividades</h1><p>Primer deploy funcionando</p>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
