a2c79647c2e91e73ebff8206e40dd0e81400ec42
epachecoosnaya <epachecoosnaya@gmail.com>
2026-04-02 21:00:21 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -650,5 +650,19 @@ def permisos_reset(uid):
     flash("Permisos restablecidos al rol por defecto","success")
     return redirect(url_for("permisos_modulo"))
 
+
+# ── ERROR HANDLERS ────────────────────────────────────────
+@app.errorhandler(403)
+def error_403(e):
+    return render_template("403.html"), 403
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def error_500(e):
+    return render_template("404.html"), 500
+
 if __name__ == "__main__":
     app.run(debug=True)
